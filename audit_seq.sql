53973cfb7b7a64232fcd608ad941ef8290b7492b
benjamintaya0111 <benjamintaya0111@gmail.com>
2018-03-01 13:31:53 +0000

    Sequence for Audit

---
+++ audit_seq.sql
@@ -0,0 +1,8 @@
+--------------------------------------------------------
+--  File created - Thursday-March-01-2018   
+--------------------------------------------------------
+--------------------------------------------------------
+--  DDL for Sequence AUDIT_TRAIL
+--------------------------------------------------------
+
+   CREATE SEQUENCE  "C3444086"."AUDIT_TRAIL"  MINVALUE 1 MAXVALUE 9999999999999999999999999999 INCREMENT BY 1 START WITH 272 CACHE 20 NOORDER  NOCYCLE  NOKEEP  NOSCALE  GLOBAL ;
