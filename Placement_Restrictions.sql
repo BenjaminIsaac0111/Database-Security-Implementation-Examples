40d0592c99285d0992fb870e1f06aa742555a505
benjamintaya0111 <benjamintaya0111@gmail.com>
2018-03-01 00:47:31 +0000

    Ajustment for the Requirments

--- Placement_Restrictions.sql
+++ Placement_Restrictions.sql
@@ -1,5 +1,5 @@
 --------------------------------------------------------
---  File created - Wednesday-February-28-2018   
+--  File created - Thursday-March-01-2018   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Trigger PLACEMENT_RESTRICTIONS
@@ -7,10 +7,12 @@
 
   CREATE OR REPLACE EDITIONABLE TRIGGER "C3444086"."PLACEMENT_RESTRICTIONS" FOR INSERT OR
   UPDATE ON LDS_PLACEMENT COMPOUND TRIGGER 
+
+
   username VARCHAR2(255);
   daily_insert_amount NUMBER;
-  --Set the variables up.
-  BEFORE STATEMENT
+  --Set the variable states up for comparisons.
+BEFORE STATEMENT
 IS
 BEGIN
   SELECT cst_name INTO username FROM lds_consultant WHERE cst_name = USER;
@@ -21,6 +23,8 @@ BEGIN
   AND DATESTAMP = TO_CHAR(SYSDATE, 'MM/DD/YYYY')
   AND USER      = USERNAME;
 END BEFORE STATEMENT;
+
+
 --Make sure that all parameters are met so that a user may modify the table.
 BEFORE EACH ROW
 IS
@@ -38,12 +42,14 @@ BEGIN
     END IF;
   END IF;
   
-  IF UPDATING AND :OLD.ptl_actual_start_date IS NULL THEN
+  IF UPDATING AND :OLD.PLT_ACTUAL_END_DATE IS NULL THEN
     DBMS_OUTPUT.put_line(username || ': Success!');
   ELSE
     RAISE_APPLICATION_ERROR(-20003, 'Not an open placement, modification denied.');
   END IF;
 END BEFORE EACH ROW;
+
+
 END;
 /
 ALTER TRIGGER "C3444086"."PLACEMENT_RESTRICTIONS" ENABLE;
